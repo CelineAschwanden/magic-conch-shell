bc2b08e84fb2530f18773d9da58422fabba5efa5
Ciliscu <ciliscu@protonmail.com>
2022-04-11 16:51:35 +0200

    initialized project

---
+++ src-backend/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({
+        icon: path.join(__dirname, "../dist/magic-conch-shell/assets/conch_shell_logo.png"),
+        webPreferences: {
+            nodeIntegration: true, // Allows IPC and other APIs
+        },
+        width: 900,
+        height: 600
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "../dist/magic-conch-shell/index.html"));
+}
+
+app.on("ready", () => {
+    createWindow();
+
+    app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './answer/answer.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:HomeComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'register', component:RegisterComponent },
+  { path: 'question', component:QuestionComponent },
+  { path: 'notifications', component:NotificationsComponent },
+  { path: 'answer', component:AnswerComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
+export const RoutingComponent = [HomeComponent,LoginComponent,RegisterComponent, QuestionComponent,
+  AnswerComponent, NotificationsComponent]
