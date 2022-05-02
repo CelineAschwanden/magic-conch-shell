66a826f229deea1a1a60b1adfb3b2be9d479e8f5
Celine Aschwanden <celine_aschwanden@sluz.ch>
2022-05-02 23:29:35 +0200

    login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({email: '', password: ''});
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  login(){
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, this.loginForm.value.email, this.loginForm.value.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   ngOnInit(): void {
   }
-
 }
